7f7fa9ea66e085af2e25bf63e6a90a5c2a9b0c60
Hejkitty54 <kozue.kishimoto@.gmail.com>
2016-02-05 10:17:07 +0100

    Initial Commit

---
+++ Fragesportsspel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fragesportsspel
+//
+//  Created by ITHS on 2016-02-05.
+//  Copyright © 2016 ITHS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fragesportsspel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fragesportsspel
+//
+//  Created by ITHS on 2016-02-05.
+//  Copyright © 2016 ITHS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
